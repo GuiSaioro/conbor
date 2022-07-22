217c375a058d66c2889098d6f53cf05953e2b218
GuiSaioro <gui.saioro@gmail.com>
2022-07-22 15:52:11 -0300

    222

--- www.cb/js/jquery.main.js
+++ www.cb/js/jquery.main.js
@@ -17,8 +17,32 @@ jQuery(function () {
 	initAnchors();
 	initCookies();
 	initacordeom();
+	empresas()
+	
 	
 });
+
+//-------------------------------------------------------------------------------
+function empresas(){
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.painel_empresas');
+
+left.addEventListener('mouseenter', () => {
+  container.classList.add('hover-left')
+})
+left.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-left')
+})
+
+
+right.addEventListener('mouseenter', () => {
+  container.classList.add('hover-right')
+})
+right.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right')
+})
+}
 
 // Função menu DropDonw imagens--------------------------------------------------------
 
