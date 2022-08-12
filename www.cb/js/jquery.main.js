9ca1f3328b02044a4b53b1a995f1a19d9d8665d7
GuiSaioro <gui.saioro@gmail.com>
2022-08-12 15:41:36 -0300

    a

--- www.cb/js/jquery.main.js
+++ www.cb/js/jquery.main.js
@@ -227,23 +227,39 @@ function modal(){
 	var modal = document.getElementById("myModal");
 	var btn = document.getElementById("btnModal");
 	var span = document.getElementsByClassName("close")[0];
+
+	var modal2 = document.getElementById("modalDownload");
+	var btn2 = document.getElementById("btn-download");
+	var span2 = document.getElementsByClassName("close2")[0];
+
 	btn.onclick = function() {
 		modal.style.display = "block"
 		
 	}
-	
+	 btn2.onclick = function() {
+		modal2.style.display = "block"
+		
+	}
 	span.onclick = function() {
 		modal.style.display = "none";
 		
 	}
-	
+	span2.onclick = function() {
+		modal2.style.display = "none";
+		
+	}
 	window.onclick = function(event) {
 	  if (event.target == modal) {
 	   modal.style.display = "none";
 	   
 	  }
+	  if (event.target == modal2) {
+		modal2.style.display = "none";
+		
+	   }
 	}
 	}
+
 /*-----------------------------------------Calculo dimensionamento AEC----------------------------------------------------*/
 
 function initacordeom() {
