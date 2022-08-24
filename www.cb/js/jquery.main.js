15bbc1d77f33e50b62eb803b3c8067e452bd80f2
GuiSaioro <gui.saioro@gmail.com>
2022-08-24 13:41:32 -0300

    uihni

--- www.cb/js/jquery.main.js
+++ www.cb/js/jquery.main.js
@@ -1,5 +1,6 @@
 // page init
 jQuery(function () {
+	initCookies();
 	initTouchNav();
 	initMobileNav();
 	initDropDownClasses();
@@ -15,14 +16,62 @@ jQuery(function () {
 	initTabs();
 	initAccordion();
 	initAnchors();
-	initCookies();
 	initacordeom();
-	empresas()
-	
 	
 });
 
 //-------------------------------------------------------------------------------
+
+function initCookies() {
+	var condition = $('#gdpr-cookies').size();
+		// && false
+ init(condition);
+	function init(condition) {
+		if (condition || condition == null) {
+			var setCookie = "";
+			var messageBlock = $("#gdpr-cookies");
+
+			setCookie = Cookies.get('gdpr-cookie');
+
+			if (isCookieSet()) {
+
+				hideMessageBlock();
+			} else {
+				showMessageBlock();
+			}
+
+			$(document).on('click', 'button.btn-accept-cookie', function (event) {
+				event.preventDefault();
+				hideMessageBlock();
+
+				Cookies.remove('gdpr-cookie');
+				Cookies.set('gdpr-cookie', 'accepted', { expires: 365 });
+			});
+
+			$(document).on('click', 'button.close-btn', function (event) {
+				hideMessageBlock();
+
+				Cookies.remove('gdpr-cookie');
+			});
+
+			function hideMessageBlock() {
+				messageBlock.hide();
+			}
+
+			function showMessageBlock() {
+				messageBlock.show();
+			}
+
+			function isCookieSet() {
+				if (setCookie == "accepted" || setCookie == "rejected") {
+					return true;
+				}
+			}
+		}
+
+	}
+}
+//-----------------------Home Empresas-------------------------------------//
 function empresas(){
 const left = document.querySelector('.esquerda');
 const right = document.querySelector('.direita');
@@ -118,55 +167,7 @@ $(window).load(function () {
 	//initMenuCropping();
 });
 
-function initCookies() {
-	var condition = $('#gdpr-cookies').size();
-		// && false
- init(condition);
-	function init(condition) {
-		if (condition || condition == null) {
-			var setCookie = "";
-			var messageBlock = $("#gdpr-cookies");
 
-			setCookie = Cookies.get('gdpr-cookie');
-
-			if (isCookieSet()) {
-
-				hideMessageBlock();
-			} else {
-				showMessageBlock();
-			}
-
-			$(document).on('click', 'button.btn-accept-cookie', function (event) {
-				event.preventDefault();
-				hideMessageBlock();
-
-				Cookies.remove('gdpr-cookie');
-				Cookies.set('gdpr-cookie', 'accepted', { expires: 365 });
-			});
-
-			$(document).on('click', 'button.close-btn', function (event) {
-				hideMessageBlock();
-
-				Cookies.remove('gdpr-cookie');
-			});
-
-			function hideMessageBlock() {
-				messageBlock.hide();
-			}
-
-			function showMessageBlock() {
-				messageBlock.show();
-			}
-
-			function isCookieSet() {
-				if (setCookie == "accepted" || setCookie == "rejected") {
-					return true;
-				}
-			}
-		}
-
-	}
-}
 
 /*--------------------------header mectrans--------------------------------------------*/
 
@@ -2019,7 +2020,7 @@ lib = {
 }(jQuery, this));
 
 
-/*  -------------------- GDPR Cookies plugin -------------------------- */
+/*  -------------------- GDPR Cookies plugin --------------------------*/ 
 
 !function (e) { var n; if ("function" == typeof define && define.amd && (define(e), n = !0), "object" == typeof exports && (module.exports = e(), n = !0), !n) { var t = window.Cookies, o = window.Cookies = e(); o.noConflict = function () { return window.Cookies = t, o } } }(function () { function e() { for (var e = 0, n = {}; e < arguments.length; e++) { var t = arguments[e]; for (var o in t) n[o] = t[o] } return n } function n(e) { return e.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent) } return function t(o) { function r() { } function i(n, t, i) { if ("undefined" != typeof document) { "number" == typeof (i = e({ path: "/" }, r.defaults, i)).expires && (i.expires = new Date(1 * new Date + 864e5 * i.expires)), i.expires = i.expires ? i.expires.toUTCString() : ""; try { var c = JSON.stringify(t); /^[\{\[]/.test(c) && (t = c) } catch (e) { } t = o.write ? o.write(t, n) : encodeURIComponent(String(t)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent), n = encodeURIComponent(String(n)).replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent).replace(/[\(\)]/g, escape); var f = ""; for (var u in i) i[u] && (f += "; " + u, !0 !== i[u] && (f += "=" + i[u].split(";")[0])); return document.cookie = n + "=" + t + f } } function c(e, t) { if ("undefined" != typeof document) { for (var r = {}, i = document.cookie ? document.cookie.split("; ") : [], c = 0; c < i.length; c++) { var f = i[c].split("="), u = f.slice(1).join("="); t || '"' !== u.charAt(0) || (u = u.slice(1, -1)); try { var a = n(f[0]); if (u = (o.read || o)(u, a) || n(u), t) try { u = JSON.parse(u) } catch (e) { } if (r[a] = u, e === a) break } catch (e) { } } return e ? r[e] : r } } return r.set = i, r.get = function (e) { return c(e, !1) }, r.getJSON = function (e) { return c(e, !0) }, r.remove = function (n, t) { i(n, "", e(t, { expires: -1 })) }, r.defaults = {}, r.withConverter = t, r }(function () { }) });
 
