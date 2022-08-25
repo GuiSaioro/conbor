325d44b3e8c7950af04e739f3590d08a5aeb5100
GuiSaioro <gui.saioro@gmail.com>
2022-08-25 14:47:14 -0300

    fvguhfvu

--- www.cb/js/jquery.main.js
+++ www.cb/js/jquery.main.js
@@ -2026,4 +2026,29 @@ function trocarIdioma(sigla) {
 		changeEvent(comboGoogleTradutor);//Dispara a troca
 	}
 }
-
+/*//////////////////////////////////////////////////////////////////////////////////////////*/
+function sliderservicos(){
+$(document).ready(function () {
+	var silder = $(".owl-carousel");
+	silder.owlCarousel({
+autoplay: true,
+autoplayTimeout: 3000,
+autoplayHoverPause: false,
+items: 1,
+stagePadding: 20,
+center: true,
+nav: false,
+margin: 50,
+dots: true,
+loop: true,
+	responsive: {
+0: {items: 1 },
+	480: {items: 2 },
+	575: {items: 2 },
+	768: {items: 2 },
+	991: {items: 3 },
+	1200: {items: 4 }
+}
+});
+});
+}
